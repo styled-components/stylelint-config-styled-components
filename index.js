995ada8c6a05099f46d21188a12042e578f8ef4e
Rakesh Menon <menonrakesh@live.com>
2017-07-28 10:35:25 +0530

    FIxed issue with missing comma for config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   "rules": {
     "value-no-vendor-prefix": true,
     "property-no-vendor-prefix": true,
-    "no-empty-source": null
+    "no-empty-source": null,
     "no-missing-end-of-source-newline": null
   }
 }
